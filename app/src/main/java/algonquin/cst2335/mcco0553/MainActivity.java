29897d3a88635103f63e774198f934c180070299
Drew-McCoy <mcco0553@algonquinlive.com>
2023-05-24 09:54:05 -0400

    for a restart

--- app/src/main/java/algonquin/cst2335/mcco0553/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mcco0553/MainActivity.java
@@ -1,15 +1,27 @@
 package algonquin.cst2335.mcco0553;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
+import algonquin.cst2335.mcco0553.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding variableBinding;
+
+    MainActivityViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView( variableBinding.getRoot() );
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button);
+
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/mcco0553/MainActivityViewModel.java
@@ -0,0 +1,7 @@
+package algonquin.cst2335.mcco0553;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends ViewModel {
+}
